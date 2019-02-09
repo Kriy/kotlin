11d808174fd0f64a26253e25580f483560a8b88f
dufan <hj2drf@163.com>
2019-02-09 21:03:46 +0800

    add

--- kotlinmvp/src/main/java/com/hazz/kotlinmvp/ui/adapter/CategoryAdapter.kt
+++ kotlinmvp/src/main/java/com/hazz/kotlinmvp/ui/adapter/CategoryAdapter.kt
@@ -1,6 +1,8 @@
 package com.hazz.kotlinmvp.ui.adapter
 
 import android.content.Context
+import android.graphics.Typeface
+import com.hazz.kotlinmvp.MyApplication
 import com.hazz.kotlinmvp.mvp.model.bean.CategoryBean
 import com.hazz.kotlinmvp.view.recyclerview.ViewHolder
 import com.hazz.kotlinmvp.view.recyclerview.adapter.CommonAdapter
@@ -10,6 +12,18 @@ import com.hazz.kotlinmvp.view.recyclerview.adapter.CommonAdapter
  */
 class CategoryAdapter(mContext: Context, categoryList: ArrayList<CategoryBean>, layoutId: Int) :
         CommonAdapter<CategoryBean>(mContext, categoryList, layoutId) {
+
+    private var textTypeFace: Typeface? = null
+
+    init {
+        textTypeFace = Typeface.createFromAsset(MyApplication.context.assets,"fonts/FZLanTingHeiS-DB1-GB-Regular.TTF")
+    }
+
+    fun setData(categoryList: ArrayList<CategoryBean>){
+        mData.clear()
+        mData = categoryList
+        notifyDataSetChanged()
+    }
 
     override fun bindData(holder: ViewHolder, data: CategoryBean, position: Int) {
 
