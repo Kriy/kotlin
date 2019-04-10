1ae358998e72ef3b610c7d6a16e5d9f20bbb9351
Kriy <hj2drf@163.com>
2019-04-10 09:53:51 +0800

    添加

--- kotlinmvp/src/main/java/com/hazz/kotlinmvp/ui/activity/VideoDetailActivity.kt
+++ kotlinmvp/src/main/java/com/hazz/kotlinmvp/ui/activity/VideoDetailActivity.kt
@@ -1,18 +1,38 @@
 package com.hazz.kotlinmvp.ui.activity
 
+import android.annotation.TargetApi
+import android.content.res.Configuration
+import android.os.Build
+import android.support.v4.view.ViewCompat
 import android.support.v7.widget.LinearLayoutManager
 import android.transition.Transition
+import android.view.View
+import android.widget.ImageView
+import com.bumptech.glide.load.DecodeFormat
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.hazz.kotlinmvp.Constants
+import com.hazz.kotlinmvp.MyApplication
 import com.hazz.kotlinmvp.R
 import com.hazz.kotlinmvp.base.BaseActivity
+import com.hazz.kotlinmvp.glide.GlideApp
 import com.hazz.kotlinmvp.mvp.contranct.VideoDetailContract
 import com.hazz.kotlinmvp.mvp.model.bean.HomeBean
 import com.hazz.kotlinmvp.mvp.presenter.VideoDetailPresenter
+import com.hazz.kotlinmvp.showToast
 import com.hazz.kotlinmvp.ui.adapter.VideoDetailAdapter
+import com.hazz.kotlinmvp.utils.CleanLeakUtils
 import com.hazz.kotlinmvp.utils.StatusBarUtil
+import com.hazz.kotlinmvp.utils.WatchHistoryUtils
+import com.hazz.kotlinmvp.view.VideoListener
+import com.orhanobut.logger.Logger
 import com.scwang.smartrefresh.header.MaterialHeader
+import com.shuyu.gsyvideoplayer.listener.LockClickListener
 import com.shuyu.gsyvideoplayer.utils.OrientationUtils
+import com.shuyu.gsyvideoplayer.video.StandardGSYVideoPlayer
+import com.shuyu.gsyvideoplayer.video.base.GSYVideoPlayer
 import kotlinx.android.synthetic.main.activity_video_detail.*
 import java.text.SimpleDateFormat
+import java.util.*
 
 class VideoDetailActivity : BaseActivity(), VideoDetailContract.View {
 
@@ -58,7 +78,236 @@ class VideoDetailActivity : BaseActivity(), VideoDetailContract.View {
         mRefreshLayout.setOnRefreshListener {
             loadVideoInfo()
         }
+        mMaterialHeader = mRefreshLayout.refreshHeader as MaterialHeader
+        mMaterialHeader?.setShowBezierWave(true)
+        mRefreshLayout.setPrimaryColorsId(R.color.color_light_black, R.color.color_title_bg)
+    }
+
+    private fun initVideoViewConfig() {
+        orientationUtils = OrientationUtils(this, mVideoView)
+        mVideoView.isRotateViewAuto = false
+        mVideoView.setIsTouchWiget(true)
+
+        val imageView = ImageView(this)
+        imageView.scaleType = ImageView.ScaleType.CENTER_CROP
+        GlideApp.with(this)
+                .load(itemData.data?.cover?.feed)
+                .centerCrop()
+                .into(imageView)
+        mVideoView.thumbImageView = imageView
+
+        mVideoView.setStandardVideoAllCallBack(object : VideoListener {
+
+            override fun onPrepared(url: String, vararg objects: Any) {
+                super.onPrepared(url, *objects)
+                orientationUtils?.isEnable = true
+                isPlay = true
+            }
+
+            override fun onAutoComplete(url: String, vararg objects: Any) {
+                super.onAutoComplete(url, *objects)
+                Logger.d("***** onAutoPlayComplete ****")
+            }
+
+            override fun onPlayError(url: String, vararg objects: Any) {
+                super.onPlayError(url, *objects)
+                showToast("播放失败")
+            }
+
+            override fun onEnterFullscreen(url: String, vararg objects: Any) {
+                super.onEnterFullscreen(url, *objects)
+                Logger.d("***** onEnterFullscreen ****")
+            }
+
+            override fun onQuitFullscreen(url: String, vararg objects: Any) {
+                super.onQuitFullscreen(url, *objects)
+                Logger.d("***** onQuitFullscreen ****")
+                orientationUtils?.backToProtVideo()
+            }
+        })
+        mVideoView.backButton.setOnClickListener({ onBackPressed() })
+        mVideoView.fullscreenButton.setOnClickListener {
+            orientationUtils?.resolveByClick()
+            mVideoView.startWindowFullscreen(this, true, true)
+        }
+        mVideoView.setLockClickListener(object : LockClickListener {
+            override fun onClick(view: View?, lock: Boolean) {
+                orientationUtils?.isEnable = !lock
+            }
+        })
+    }
+
+    override fun start() {
+
+    }
+
+    override fun initData() {
+        itemData = intent.getSerializableExtra(Constants.BUNDLE_VIDEO_DATA) as HomeBean.Issue.Item
+        isTransition = intent.getBooleanExtra(TRANSITION, false)
+        saveWatchVideoHistoryInfo(itemData)
+    }
+
+    private fun saveWatchVideoHistoryInfo(watchItem: HomeBean.Issue.Item){
+        val historyMap = WatchHistoryUtils.getAll(Constants.FILE_WATCH_HISTORY_NAME, MyApplication.context) as Map<*, *>
+        for ((key, _) in historyMap) {
+            if (watchItem == WatchHistoryUtils.getObject(Constants.FILE_WATCH_HISTORY_NAME,MyApplication.context, key as String)) {
+                WatchHistoryUtils.remove(Constants.FILE_WATCH_HISTORY_NAME,MyApplication.context, key)
+            }
+        }
+        WatchHistoryUtils.putObject(Constants.FILE_WATCH_HISTORY_NAME,MyApplication.context, watchItem,"" + mFormat.format(Date()))
+    }
+
+    override fun showLoading() {
+
+    }
+
+    override fun dismissLoading() {
+        mRefreshLayout.finishRefresh()
+    }
+
+    /**
+     * 设置播放视频 URL
+     */
+    override fun setVideo(url: String) {
+        Logger.d("playUrl:$url")
+        mVideoView.setUp(url, false, "")
+        //开始自动播放
+        mVideoView.startPlayLogic()
+    }
+
+    /**
+     * 设置视频信息
+     */
+    override fun setVideoInfo(itemInfo: HomeBean.Issue.Item) {
+        itemData = itemInfo
+        mAdapter.addData(itemInfo)
+        // 请求相关的最新等视频
+        mPresenter.requestRelatedVideo(itemInfo.data?.id?:0)
+
+    }
 
 
+    /**
+     * 设置相关的数据视频
+     */
+    override fun setRecentRelatedVideo(itemList: ArrayList<HomeBean.Issue.Item>) {
+        mAdapter.addData(itemList)
+        this.itemList = itemList
+    }
+
+
+    /**
+     * 设置背景颜色
+     */
+    override fun setBackground(url: String) {
+        GlideApp.with(this)
+                .load(url)
+                .centerCrop()
+                .format(DecodeFormat.PREFER_ARGB_8888)
+                .transition(DrawableTransitionOptions().crossFade())
+                .into(mVideoBackground)
+    }
+
+    /**
+     * 设置错误信息
+     */
+    override fun setErrorMsg(errorMsg: String) {
+
+    }
+
+
+    override fun onConfigurationChanged(newConfig: Configuration?) {
+        super.onConfigurationChanged(newConfig)
+        if (isPlay && !isPause) {
+            mVideoView.onConfigurationChanged(this, newConfig, orientationUtils)
+        }
+    }
+
+    /**
+     * 1.加载视频信息
+     */
+    fun loadVideoInfo() {
+        mPresenter.loadVideoInfo(itemData)
+    }
+
+    /**
+     * 监听返回键
+     */
+    override fun onBackPressed() {
+        orientationUtils?.backToProtVideo()
+        if (StandardGSYVideoPlayer.backFromWindowFull(this))
+            return
+        //释放所有
+        mVideoView.setStandardVideoAllCallBack(null)
+        GSYVideoPlayer.releaseAllVideos()
+        if (isTransition && Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) run {
+            super.onBackPressed()
+        } else {
+            finish()
+            overridePendingTransition(R.anim.anim_out, R.anim.anim_in)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        getCurPlay().onVideoResume()
+        isPause = false
+    }
+
+    override fun onPause() {
+        super.onPause()
+        getCurPlay().onVideoPause()
+        isPause = true
+    }
+
+    override fun onDestroy() {
+        CleanLeakUtils.fixInputMethodManagerLeak(this)
+        super.onDestroy()
+        GSYVideoPlayer.releaseAllVideos()
+        orientationUtils?.releaseListener()
+        mPresenter.detachView()
+    }
+
+    private fun getCurPlay(): GSYVideoPlayer {
+        return if (mVideoView.fullWindowPlayer != null) {
+            mVideoView.fullWindowPlayer
+        } else mVideoView
+    }
+
+    private fun initTransition() {
+        if (isTransition && Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            postponeEnterTransition()
+            ViewCompat.setTransitionName(mVideoView, IMG_TRANSITION)
+            addTransitionListener()
+            startPostponedEnterTransition()
+        } else {
+            loadVideoInfo()
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private fun addTransitionListener() {
+        transition = window.sharedElementEnterTransition
+        transition?.addListener(object : Transition.TransitionListener {
+            override fun onTransitionResume(p0: Transition?) {
+            }
+
+            override fun onTransitionPause(p0: Transition?) {
+            }
+
+            override fun onTransitionCancel(p0: Transition?) {
+            }
+
+            override fun onTransitionStart(p0: Transition?) {
+            }
+
+            override fun onTransitionEnd(p0: Transition?) {
+                Logger.d("onTransitionEnd()------")
+
+                loadVideoInfo()
+                transition?.removeListener(this)
+            }
+
+        })
     }
 }
