a8db2702623ca040fa8ff1da9473aaf26217e9d6
Kriy <hj2drf@163.com>
2019-04-12 20:55:04 +0800

    添加

--- kotlinmvp/src/main/java/com/hazz/kotlinmvp/ui/fragment/HomeFragment.kt
+++ kotlinmvp/src/main/java/com/hazz/kotlinmvp/ui/fragment/HomeFragment.kt
@@ -1,13 +1,25 @@
 package com.hazz.kotlinmvp.ui.fragment
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.support.v4.app.ActivityOptionsCompat
+import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import com.hazz.kotlinmvp.R
 import com.hazz.kotlinmvp.base.BaseFragment
 import com.hazz.kotlinmvp.mvp.contranct.HomeContract
+import com.hazz.kotlinmvp.mvp.model.bean.HomeBean
 import com.hazz.kotlinmvp.mvp.presenter.HomePresenter
+import com.hazz.kotlinmvp.net.exception.ErrorStatus
+import com.hazz.kotlinmvp.showToast
 import com.hazz.kotlinmvp.ui.adapter.HomeAdapter
+import com.hazz.kotlinmvp.utils.StatusBarUtil
+import com.orhanobut.logger.Logger
 import com.scwang.smartrefresh.header.MaterialHeader
+import kotlinx.android.synthetic.main.frament_home.*
+import kotlinx.android.synthetic.main.item_home_header.*
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -46,4 +58,135 @@ class HomeFragment : BaseFragment(), HomeContract.View {
 
     override fun getLayoutId(): Int = R.layout.frament_home
 
+    override fun initView() {
+        mPresenter.attachView(this)
+        mRefreshLayout.setEnableHeaderTranslationContent(true)
+        mRefreshLayout.setOnRefreshListener {
+            isRefresh = true
+            mPresenter.requestHomeData(num)
+        }
+        mMaterialHeader = mRefreshLayout.refreshHeader as MaterialHeader
+        mMaterialHeader?.setShowBezierWave(true)
+        mRefreshLayout.setPrimaryColorsId(R.color.color_light_black, R.color.color_title_bg)
+
+        mRecyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView?, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    val childCount = RecyclerView.SCROLL_STATE_IDLE
+                    val itemCount = mRecyclerView.layoutManager.itemCount
+                    val firstVisibleItem = (mRecyclerView.layoutManager as LinearLayoutManager).findFirstVisibleItemPosition()
+                    if (firstVisibleItem + childCount == itemCount) {
+                        if (!loadingMore) {
+                            loadingMore = true
+                            mPresenter.loadMoreData()
+                        }
+                    }
+                }
+            }
+
+            override fun onScrolled(recyclerView: RecyclerView?, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                val currentVisibleItemPosition = linearLayoutManager.findFirstVisibleItemPosition()
+                if (currentVisibleItemPosition == 0) {
+                    toolbar.setBackgroundColor(getColor(R.color.color_translucent))
+                    iv_search.setImageResource(R.mipmap.ic_action_search_white)
+                    tv_header_title.text = ""
+                } else {
+                    if (mHomeAdapter?.mData!!.size > 1) {
+                        toolbar.setBackgroundColor(getColor(R.color.color_title_bg))
+                        iv_search.setImageResource(R.mipmap.ic_action_search_black)
+                        val itemList = mHomeAdapter!!.mData
+                        val item = itemList[currentVisibleItemPosition + mHomeAdapter!!.bannerItemSize - 1]
+                        if (item.type == "textHeader") {
+                            tv_header_title.text = item.data?.text
+                        } else {
+                            tv_header_title.text = simpleDateFormat.format(item.data?.date)
+                        }
+                    }
+                }
+            }
+        })
+        iv_search.setOnClickListener { openSearchActivity() }
+        mLayoutStatusView = multipleStatusView
+        activity?.let { StatusBarUtil.darkMode(it) }
+        activity?.let { StatusBarUtil.setPaddingSmart(it, toolbar) }
+    }
+
+    private fun openSearchActivity() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val options = activity?.let { ActivityOptionsCompat.makeSceneTransitionAnimation(it, iv_search, iv_search.transitionName) }
+            startActivity(Intent(activity, SearchActivity::class.java), options?.toBundle())
+        } else {
+            startActivity(Intent(activity, SearchActivity::class.java))
+        }
+    }
+
+    override fun lazyLoad() {
+        mPresenter.requestHomeData(num)
+    }
+
+
+    /**
+     * 显示 Loading （下拉刷新的时候不需要显示 Loading）
+     */
+    override fun showLoading() {
+        if (!isRefresh) {
+            isRefresh = false
+            mLayoutStatusView?.showLoading()
+        }
+    }
+
+    /**
+     * 隐藏 Loading
+     */
+    override fun dismissLoading() {
+        mRefreshLayout.finishRefresh()
+    }
+
+    /**
+     * 设置首页数据
+     */
+    override fun setHomeData(homeBean: HomeBean) {
+        mLayoutStatusView?.showContent()
+        Logger.d(homeBean)
+
+        // Adapter
+        mHomeAdapter = activity?.let { HomeAdapter(it, homeBean.issueList[0].itemList) }
+        //设置 banner 大小
+        mHomeAdapter?.setBannerSize(homeBean.issueList[0].count)
+
+        mRecyclerView.adapter = mHomeAdapter
+        mRecyclerView.layoutManager = linearLayoutManager
+        mRecyclerView.itemAnimator = DefaultItemAnimator()
+
+    }
+
+    override fun setMoreData(itemList: ArrayList<HomeBean.Issue.Item>) {
+        loadingMore = false
+        mHomeAdapter?.addItemData(itemList)
+    }
+
+
+    /**
+     * 显示错误信息
+     */
+    override fun showError(msg: String, errorCode: Int) {
+        showToast(msg)
+        if (errorCode == ErrorStatus.NETWORK_ERROR) {
+            mLayoutStatusView?.showNoNetwork()
+        } else {
+            mLayoutStatusView?.showError()
+        }
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mPresenter.detachView()
+    }
+
+    fun getColor(colorId: Int): Int {
+        return resources.getColor(colorId)
+    }
 }
